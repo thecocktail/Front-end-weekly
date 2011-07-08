48d50eec2632fe03fd94cd13c17a23c7dc7e634e
vieron <javiersanchezmarin@gmail.com>
2011-07-08 09:52:18 +0200

    first issue

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,32 +1,6 @@
 include Nanoc3::Helpers::Rendering
 include Nanoc3::Helpers::Capturing
 
-def current? id, wrap = nil
-  if @item[:nav_id] == id || @item[:sub_nav_id] == id
-    wrap.nil? ? 'active' : %~#{wrap}="active"~
-  end
-end
-
-def select_month_options
-  index = 0
-  ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"].inject("") do |output,member|
-    output << %~<option value="#{index = index + 1}">#{member}</option>~
-  end
-end
-
-def select_days_options
-  (1..31).inject("") do |output,member|
-    output << %~<option value="#{member}">#{member}</option>~
-  end
-end
-
-def select_years_options
-  initial_year = 2010
-  (0..10).inject("") do |output,member|
-    output << %~<option value="#{initial_year = initial_year + 1}">#{initial_year}</option>~
-  end
-end
-
-def link text, href, br = false
-  %~ <a href="#{href}" style="color:#338AC4; line-height:24px;">#{text}</a> #{"<br />" if br }~
+def link href, text = false, br = false
+  %~ <a href="#{href}" style="color:#338AC4; line-height:24px;">#{text ? text : href}</a> #{"<br />" if br }~
 end
