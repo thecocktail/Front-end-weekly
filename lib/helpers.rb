9a49e4ab885faefaeda6a76e7bb1b3bef7a68b2d
Natalí <natali.sanchez@the-cocktail.com>
2011-07-08 10:55:27 +0200

    cambios de textos issue 1

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -2,5 +2,5 @@ include Nanoc3::Helpers::Rendering
 include Nanoc3::Helpers::Capturing
 
 def link href, text = false, br = false
-  %~ <a href="#{href}" style="color:#338AC4; line-height:24px;">#{text ? text : href}</a> #{"<br />" if br }~
+  %~ <a href="#{href}" style="color:#338AC4; line-height:24px;">#{text ? text : href}</a>#{"<br />" if br }~
 end
